1244d85bd8bba959ea5dad136478f69d18b6e389
UnclassedPenguin <tvanden@live.com>
2022-01-02 14:44:25 -0700

    add comments

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+# Written by Tyler(UnclassedPenguin) 2022
+# Site: https://unclassed.ca
+# Github: https://github.com/UnclassedPenguin
+#
 
-basedir=~/git/                          # Where you want to put dotfiles_old/
-dir=~/git/dotfiles
-budir=~/git/dotfiles_old
-files="i3status.conf vimrc Xresources zshrc"
-i3conf=i3config
+basedir=~/git/                                  # Where you want to put dotfiles_old/
+dir=~/git/dotfiles                              # Your dotfiles directory
+budir=~/git/dotfiles_old                        # dotfiles_old directory
+files="i3status.conf vimrc Xresources zshrc"    # Files to symlink
+i3conf=i3config                                 # ~/.config/i3/config name in backup/dotfiles
 
+# Create backup directory
 echo "Creating $budir for backup of existing files in $basedir"
 mkdir -p $budir
 echo "...done"
 
+# Change to backup directory, to copy files here
 echo "Changing to $dir directory"
 cd $dir
 echo "...done"
 
+# Copy all the existing dotfiles into the backup dir, 
+# and create symlinks to dotfiles in your git dotfiles folder. 
 for file in $files; do
   echo "Backing up $file to $budir"
   mv ~/.$file $budir/$file
@@ -23,6 +31,9 @@ for file in $files; do
   echo "...done"
 done
 
+# Because the i3config file is in a different place I put it here seperately.
+# This just does the same thing, backs up the existing file to dotfiles_old,
+# and then creates a symlink from dotfiles/i3config. 
 echo "Backing up $i3conf to $budir"
 mv ~/.config/i3/config $budir/i3config
 echo "...done"
--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+# Written by Tyler(UnclassedPenguin) 2022
+# Site: https://unclassed.ca
+# Github: https://github.com/UnclassedPenguin
+#
 
-dir=~/git/dotfiles
-budir=~/git/dotfiles_old
-files="i3status.conf vimrc Xresources zshrc"
-i3conf=i3config
+# Variables
+dir=~/git/dotfiles                              # Your dotfiles directory
+budir=~/git/dotfiles_old                        # dotfiles_old directory
+files="i3status.conf vimrc Xresources zshrc"    # Files to unsymlink
+i3conf=i3config                                 # ~/.config/i3/config name in backup and dotfiles
 
+
+# Change directory to backup directory
 echo "Moving to backup directory: $budir"
 cd $budir
 echo "...done"
 
+# Delete symlinks and move backed up version back to original place
 for file in $files; do
   echo "Deleting $file symlink"
   rm ~/.$file
@@ -18,6 +26,8 @@ for file in $files; do
   echo "...done"
 done
 
+# Same as above, but different location. Deletes symlink for i3config
+# at ~/.config/i3/config and moves back the original file
 echo "Deleting $i3conf symlink"
 rm ~/.config/i3/config
 echo "...done"
