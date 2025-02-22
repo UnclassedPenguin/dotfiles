3dfc4b595b36038dca47faec6e5e1221f0531682
Tyler <tvanden@live.com>
2025-02-21 19:45:37 -0700

    - add alias.zsh to zsh - add pwdandls to misc

---
+++ zsh/alias.zsh
@@ -0,0 +1,9 @@
+alias x="exit"
+alias q="exit"
+alias n="newsboat"
+alias upgrade="sudo apt update && sudo apt upgrade"
+alias fv="fzf --height 40% --layout reverse --border --bind 'enter:become(vim {})'"
+
+# ls when you cd
+# pwdandls is in ~/.bin
+chpwd() pwdandls
